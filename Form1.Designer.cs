2738f1a3a67b015cc5aa92f7bb2d3b11a11f4985
Daniele Alberti <kadmos@kadmos.com>
2021-11-04 12:10:40 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace ShowRubberLine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DXFReaderNET.DxfDocument dxfDocument2 = new DXFReaderNET.DxfDocument();
+            DXFReaderNET.AciColor aciColor2 = new DXFReaderNET.AciColor();
+            DXFReaderNET.Objects.RasterVariables rasterVariables2 = new DXFReaderNET.Objects.RasterVariables();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dxfReaderNETControl1 = new DXFReaderNET.DXFReaderNETControl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // dxfReaderNETControl1
+            // 
+            this.dxfReaderNETControl1.AutoSize = true;
+            this.dxfReaderNETControl1.CustomCursor = DXFReaderNET.CustomCursorType.None;
+            dxfDocument2.ActiveLayout = "Model";
+            dxfDocument2.Comments = null;
+            aciColor2.Index = ((short)(256));
+            aciColor2.IsByBlock = false;
+            aciColor2.IsByLayer = true;
+            aciColor2.UseTrueColor = false;
+            dxfDocument2.CurrentColor = aciColor2;
+            dxfDocument2.CurrentElevation = 0D;
+            dxfDocument2.CurrentLayer = "0";
+            dxfDocument2.CurrentLineTypeName = "ByLayer";
+            dxfDocument2.CurrentLineTypeScale = 1D;
+            dxfDocument2.CurrentTextSize = 2.5D;
+            dxfDocument2.CurrentTextStyle = "STANDARD";
+            dxfDocument2.CurrentThickness = 0D;
+            dxfDocument2.DateCreated = new System.DateTime(2020, 1, 29, 10, 1, 45, 494);
+            dxfDocument2.DateLastEdited = new System.DateTime(2020, 1, 29, 10, 1, 45, 494);
+            dxfDocument2.Handle = "0";
+            dxfDocument2.IsBinary = false;
+            dxfDocument2.Modified = false;
+            dxfDocument2.Name = null;
+            dxfDocument2.Owner = null;
+            rasterVariables2.DisplayFrame = true;
+            rasterVariables2.DisplayQuality = DXFReaderNET.Objects.ImageDisplayQuality.High;
+            rasterVariables2.Handle = "2D";
+            rasterVariables2.Owner = null;
+            rasterVariables2.Units = DXFReaderNET.ImageUnits.Unitless;
+            dxfDocument2.RasterVariables = rasterVariables2;
+            dxfDocument2.ThumbnailImage = null;
+            dxfDocument2.TotalEditTime = new System.DateTime(2020, 1, 29, 0, 0, 0, 0);
+            dxfDocument2.TotalEditTimeDays = 0;
+            dxfDocument2.UserTimer = new System.DateTime(2020, 1, 29, 0, 0, 0, 0);
+            dxfDocument2.UserTimerDays = 0;
+            this.dxfReaderNETControl1.DXF = dxfDocument2;
+            this.dxfReaderNETControl1.FileName = null;
+            this.dxfReaderNETControl1.HighlightMarkerColor2 = System.Drawing.Color.Magenta;
+            this.dxfReaderNETControl1.Location = new System.Drawing.Point(12, 38);
+            this.dxfReaderNETControl1.Name = "dxfReaderNETControl1";
+            this.dxfReaderNETControl1.ShowGrid = true;
+            this.dxfReaderNETControl1.ShowGridRuler = false;
+            this.dxfReaderNETControl1.Size = new System.Drawing.Size(788, 446);
+            this.dxfReaderNETControl1.TabIndex = 0;
+            this.dxfReaderNETControl1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.dxfReaderNETControl1_MouseMove);
+            this.dxfReaderNETControl1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.dxfReaderNETControl1_MouseUp);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(174, 20);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Add line";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(809, 493);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dxfReaderNETControl1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "DXFReader.NET Component - ShowRubberBand Sample ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DXFReaderNET.DXFReaderNETControl dxfReaderNETControl1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Windows.Forms;
+using DXFReaderNET;
+using DXFReaderNET.Entities;
+namespace ShowRubberLine
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        internal enum FunctionsEnum
+        {
+            None,
+            Line1,
+            Line2,
+        }
+
+        private Vector2 p1 = Vector2.Zero;
+        private Vector2 p2 = Vector2.Zero;
+        private FunctionsEnum CurrentFunction = FunctionsEnum.None;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CurrentFunction = FunctionsEnum.Line1;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dxfReaderNETControl1.NewDrawing();
+            dxfReaderNETControl1.CustomCursor = CustomCursorType.CrossHair;
+        }
+
+        private void dxfReaderNETControl1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                switch (CurrentFunction)
+                {
+                    case FunctionsEnum.Line2:
+                        CurrentFunction = FunctionsEnum.None;
+
+                        p2 = dxfReaderNETControl1.CurrentWCSpoint;
+
+                        dxfReaderNETControl1.DrawEntity(dxfReaderNETControl1.AddLine(p1.ToVector3(), p2.ToVector3()));
+
+                        break;
+                    case FunctionsEnum.Line1:
+                        CurrentFunction = FunctionsEnum.Line2;
+
+                        p1 = dxfReaderNETControl1.CurrentWCSpoint;
+                        break;
+                }
+
+            }
+        }
+
+        private void dxfReaderNETControl1_MouseMove(object sender, MouseEventArgs e)
+        {
+            switch (CurrentFunction)
+            {
+                case FunctionsEnum.Line2:
+                    dxfReaderNETControl1.ShowRubberBandLine(p1, dxfReaderNETControl1.CurrentWCSpoint);
+                    break;
+            }
+        }
+        
+    }
+}
+
